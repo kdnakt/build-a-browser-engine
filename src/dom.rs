70c8c0dcf8aa20be190cc1f7983804164ceb221a
kdnakt <a.kid.1985@gmail.com>
2024-02-20 09:48:38 +0900

    add Element

--- src/dom.rs
+++ src/dom.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Node {
@@ -8,11 +9,30 @@ pub struct Node {
 #[derive(Debug)]
 pub enum NodeType {
     Text(String),
+    Element(ElementData),
+}
+
+pub type AttrMap = HashMap<String, String>;
+
+#[derive(Debug)]
+pub struct ElementData {
+    tag_name: String,
+    attributes: AttrMap,
 }
 
 pub fn text(data: String) -> Node {
     Node {
         children: Vec::new(),
         node_type: NodeType::Text(data)
+    }
+}
+
+pub fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children: children,
+        node_type: NodeType::Element(ElementData {
+            tag_name: name,
+            attributes: attrs,
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 mod dom;
 
 fn main() {
@@ -5,4 +7,18 @@ fn main() {
 
     let text = dom::text("Hello World".to_string());
     println!("{:?}", text);
+
+    let mut attrs = HashMap::new();
+    attrs.insert(
+        "lang".to_string(),
+        "en".to_string()
+    );
+    attrs.insert(
+        "data-theme".to_string(),
+        "dark".to_string()
+    );
+    let mut children = Vec::new();
+    children.push(text);
+    let html = dom::elem("html".to_string(), attrs, children);
+    println!("{:?}", html);
 }
