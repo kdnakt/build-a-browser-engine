a9f99e4b55963eca3f393b403be8430913f146bf
kdnakt <a.kid.1985@gmail.com>
2024-03-03 12:06:01 +0900

    add Specificity

--- src/css.rs
+++ src/css.rs
@@ -152,3 +152,15 @@ fn valid_identifier_char(c: char) -> bool {
         _ => false,
     }
 }
+
+pub type Specificity = (usize, usize, usize);
+
+impl Selector {
+    pub fn specificity(&self) -> Specificity {
+        let Selector::Simple(ref simple) = *self;
+        let a = simple.id.iter().count();
+        let b = simple.class.len();
+        let c = simple.tag_name.iter().count();
+        (a, b, c)
+    }
+}
