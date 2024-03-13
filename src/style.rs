1f2eb203bef6c3b7a4966b8e2752c933d0f14593
kdnakt <a.kid.1985@gmail.com>
2024-03-13 10:25:00 +0900

    style::matches()

--- src/style.rs
+++ src/style.rs
@@ -1,6 +1,14 @@
 use std::collections::HashMap;
-use crate::css::Value;
-use crate::dom::Node;
+use crate::css::{
+    Selector,
+    Selector::Simple,
+    SimpleSelector,
+    Value,
+};
+use crate::dom::{
+    ElementData,
+    Node,
+};
 
 /// Map from CSS property names to values.
 type PropertyMap = HashMap<String, Value>;
@@ -10,4 +18,14 @@ struct StyledNode<'a> {
     node: &'a Node,
     specified_values: PropertyMap,
     children: Vec<StyledNode<'a>>,
+}
+
+fn matches(elem: &ElementData, selector: &Selector) -> bool {
+    match *selector {
+        Simple(ref simple_selector) => matches_simple_selector(elem, simple_selector)
+    }
+}
+
+fn matches_simple_selector(elem: &ElementData, selector: &SimpleSelector) -> bool {
+    todo!()
 }
