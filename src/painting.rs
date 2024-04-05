87256d64cee5977ae84430d9fde3b4dc19fc23e5
kdnakt <a.kid.1985@gmail.com>
2024-04-05 10:17:42 +0900

    build_display_list()

--- src/painting.rs
+++ src/painting.rs
@@ -1,8 +1,21 @@
 use crate::css::Color;
-use crate::layout::Rect;
+use crate::layout::{
+    LayoutBox,
+    Rect,
+};
 
 type DisplayList = Vec<DisplayCommand>;
 
 enum DisplayCommand {
     SolidColor(Color, Rect),
+}
+
+fn build_display_list(layout_root: &LayoutBox) -> DisplayList {
+    let mut list = Vec::new();
+    render_layout_box(&mut list, layout_root);
+    return list;
+}
+
+fn render_layout_box(list: &mut DisplayList, layout_box: &LayoutBox) {
+    todo!();
 }
