39ab977b35adf618be49b956b9bd6ba6d5b646ea
kdnakt <a.kid.1985@gmail.com>
2024-04-10 00:25:47 +0900

    paint()

--- src/painting.rs
+++ src/painting.rs
@@ -117,3 +117,12 @@ impl Canvas {
         }
     }
 }
+
+fn paint(layout_root: &LayoutBox, bounds: Rect) -> Canvas {
+    let display_list = build_display_list(layout_root);
+    let mut canvas = Canvas::new(bounds.width as usize, bounds.height as usize);
+    for item in display_list {
+        canvas.paint_item(&item);
+    }
+    canvas
+}
